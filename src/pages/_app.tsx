d32169094c2a28851a2a6fb45fe15f900551b20f
Seyeon-Song <seyeon.song@bmeks.co.kr>
2024-09-05 17:32:19 +0900

    feat: 리액트 쿼리 사용을 위한 설정
    #3

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,20 @@
 import Layout from "@/components/Layout";
 import MuiLocalizationProvider from "@/lib/utils/muiLocalizationProvider";
 import MuiThemeProvider from "@/lib/utils/muiThemeProvider";
+import ReactQueryProvider from "@/lib/utils/reactQueryProvider";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MuiLocalizationProvider>
-      <MuiThemeProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </MuiThemeProvider>
-    </MuiLocalizationProvider>
+    <ReactQueryProvider>
+      <MuiLocalizationProvider>
+        <MuiThemeProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </MuiThemeProvider>
+      </MuiLocalizationProvider>
+    </ReactQueryProvider>
   );
 }
