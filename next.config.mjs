7215da30aff6ad45963544d4b40e48740951a928
Seyeon-Song <seyeon.song@bmeks.co.kr>
2024-09-06 15:46:52 +0900

    feat: api 요청 base url 설정
    #3

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/soil_psorms_api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL_INTERNAL}${process.env.NEXT_PUBLIC_PSORMS_API_PATH}/:path*`,
+        basePath: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
