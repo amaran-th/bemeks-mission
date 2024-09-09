3cc8e59952ee04ddc746fb765d6e4e60f7f70e7a
amaran-th <songsy405@naver.com>
2024-09-09 16:47:04 +0900

    fix: api 호출 시 '전체' 키워드 제외시키도록 하기
    #3

--- src/lib/utils/utils.ts
+++ src/lib/utils/utils.ts
@@ -1,7 +1,7 @@
 export const removeEmptyField = <T extends Record<string, unknown>>(obj: T) =>
   Object.fromEntries(
     Object.entries(obj).filter(([, v]) => {
-      if (v === null || v === undefined || v === "" || v === 0) {
+      if (v === null || v === undefined || v === "" || v === "전체") {
         // TODO: 0 조건 제거하기
         return false;
       }
